be390bfca747d778426102992fab68627170da21
Bhavesh Ala <bala@xceleratorgroup.com>
2024-05-22 12:09:40 -0400

    "Second Commit"

--- base/tasks.py
+++ base/tasks.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from studybud import settings
 
-@shared_task(bind=True, time_limit=5)
+@shared_task(bind=True, time_limit=1)
 def send_mail_func(self, subject, message, email):
     send_mail(
         subject=subject,
